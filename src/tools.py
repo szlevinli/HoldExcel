9aea1657b1d121396751c7c1cbcdb1ec3b7dd840
levin <3259963@qq.com>
2020-01-31 20:38:13 +0800

    增加三种从Excel中提取数据的方法

--- src/tools.py
+++ src/tools.py
@@ -30,7 +30,7 @@ def get_template():
     return load_workbook(get_template_file_full_path(), read_only=True)
 
 
-def make_dict_from_3cloumns(wb, sheet_name):
+def make_dict_repeatable(wb, sheet_name):
     ws = wb[sheet_name]
     dicts = {}
     rows = []
@@ -41,6 +41,49 @@ def make_dict_from_3cloumns(wb, sheet_name):
     keys = rows[0]
     for row in rows[1:]:
         tmp1 = dicts[row[0]] if row[0] in dicts else []
-        tmp1.append({keys[1]: row[1], keys[2]: row[2]})
+        tmp2 = {}
+        for k, v in enumerate(row[1:]):
+            tmp2[keys[k+1]] = v
+        tmp1.append(tmp2)
         dicts[row[0]] = tmp1
     return dicts
+
+
+def make_dict_unrepeatable(wb, sheet_name):
+    ws = wb[sheet_name]
+    rows = []
+    for v in ws.iter_rows(min_row=ws.min_row, max_row=ws.max_row,
+                          min_col=ws.min_column, max_col=ws.max_column,
+                          values_only=True):
+        rows.append(v)
+    dicts = {}
+    # 第一行是配置项名称
+    keys = rows[0]
+    # 循环配置信息，排除第一行的配置项名称
+    for row in rows[1:]:
+        tmp = {}
+        # 循环配置项，排除第一个配置项
+        # 第一个配置项是 configID，将作为 key
+        for k, v in enumerate(row[1:]):
+            tmp[keys[k+1]] = v
+        dicts[row[0]] = tmp
+    return dicts
+
+
+def make_list(wb, sheet_name):
+    ws = wb[sheet_name]
+    rows = []
+    for v in ws.iter_rows(min_row=ws.min_row, max_row=ws.max_row,
+                          min_col=ws.min_column, max_col=ws.max_column,
+                          values_only=True):
+        rows.append(v)
+    lists = []
+    # 第一行是配置项名称
+    keys = rows[0]
+    # 循环配置信息，排除第一行的配置项名称
+    for row in rows[1:]:
+        tmp = {}
+        for k, v in enumerate(row):
+            tmp[keys[k]] = v
+        lists.append(tmp)
+    return lists
--- src/tools_test.py
+++ src/tools_test.py
@@ -1,7 +1,11 @@
-from tools import make_dict_from_3cloumns
+from tools import (
+    make_dict_repeatable,
+    make_dict_unrepeatable,
+    make_list
+)
 
 
-def test_make_dict_from_3cloumns(mocker, make_wb_and_ws):
+def test_make_dict_repeatable(mocker, make_wb_and_ws):
     return_value = [
         ('id', 'From', 'To'),
         (1, 'A', 'F'),
@@ -28,5 +32,53 @@ def test_make_dict_from_3cloumns(mocker, make_wb_and_ws):
     sheet_name = 'Temp'
     wb, ws = make_wb_and_ws(sheet_name)
     mocker.patch.object(ws, 'iter_rows', return_value=return_value)
-    dicts = make_dict_from_3cloumns(wb, sheet_name)
+    dicts = make_dict_repeatable(wb, sheet_name)
     assert dicts == expect_value
+
+
+def test_make_dict_unrepeatable(mocker, make_wb_and_ws):
+    return_value = [
+        ('ConfigID', 'FileName', 'SheetName'),
+        (1, 'filename1', 'sheetname1'),
+        (2, 'filename1', 'sheetname2'),
+        (3, 'filename2', 'sheetname1'),
+        (1, 'repeatK1', 'repeatK2')
+    ]
+    expect_value = {
+        1: {'FileName': 'repeatK1', 'SheetName': 'repeatK2'},
+        2: {'FileName': 'filename1', 'SheetName': 'sheetname2'},
+        3: {'FileName': 'filename2', 'SheetName': 'sheetname1'}
+    }
+    sheet_name = 'Temp'
+    wb, ws = make_wb_and_ws(sheet_name)
+    mocker.patch.object(ws, 'iter_rows', return_value=return_value)
+    dicts = make_dict_unrepeatable(wb, sheet_name)
+    assert dicts == expect_value
+
+
+def test_make_list(mocker, make_wb_and_ws):
+    return_value = [
+        ('ConfigID', 'ColumnName1', 'ColumnName2', 'ColumnName3'),
+        ('1', '100', '200', '300'),
+        ('1', '1000', '2000', '3000'),
+        ('2', '10000', '20000', '30000')
+    ]
+    expect_value = [
+        {
+            'ConfigID': '1', 'ColumnName1': '100',
+            'ColumnName2': '200', 'ColumnName3': '300'
+        },
+        {
+            'ConfigID': '1', 'ColumnName1': '1000',
+            'ColumnName2': '2000', 'ColumnName3': '3000'
+        },
+        {
+            'ConfigID': '2', 'ColumnName1': '10000',
+            'ColumnName2': '20000', 'ColumnName3': '30000'
+        },
+    ]
+    sheet_name = 'Temp'
+    wb, ws = make_wb_and_ws(sheet_name)
+    mocker.patch.object(ws, 'iter_rows', return_value=return_value)
+    lists = make_list(wb, sheet_name)
+    assert lists == expect_value
