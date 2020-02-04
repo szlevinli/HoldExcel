624669523ee602207bedac8162b6bffa7dea1122
levin <3259963@qq.com>
2020-02-04 09:17:23 +0800

    添加生成共享 yaml config 数据的 fixture

--- src/conftest.py
+++ src/conftest.py
@@ -1,11 +1,24 @@
 import pytest
+import global_var as GL
+
 
 @pytest.fixture
 def make_wb_and_ws():
     from openpyxl import Workbook
     wb = Workbook()
     ws = wb.active
+
     def _make_wb_and_ws(title):
         ws.title = title
         return wb, ws
     return _make_wb_and_ws
+
+
+@pytest.fixture
+def make_yaml_config():
+    return {
+        GL.GL_CONFIG_YAML_KEY_TEMPLATE_DIR: 'test_template_dir',
+        GL.GL_CONFIG_YAML_KEY_DATA_DIR: 'test_data_dir',
+        GL.GL_CONFIG_YAML_KEY_OUT_DIR: 'test_out_dir',
+        GL.GL_CONFIG_YAML_KEY_TEMPLATE_FILE_NAME: 'test_template_file_name'
+    }
