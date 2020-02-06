204f4709347a896b62fcd4dbd847034f5eec8bfc
levin <3259963@qq.com>
2020-02-06 21:03:56 +0800

    消除 hard code

--- src/generate_map.py
+++ src/generate_map.py
@@ -1,4 +1,6 @@
 from tools import make_dict_repeatable
+import global_var as GL
+
 
 def get_maps(wb):
     """获取映射关系
@@ -6,14 +8,14 @@ def get_maps(wb):
     三列，分别表示：ConfigID, From, To
     返回的映射关系为字典类型，结构为：
     {ConfigID: [{'From': 'A', 'To': 'B'}, ...]}
-    
+
     Args:
         wb (openpyxl.Workbook): 模板文件（excel）
-    
+
     Returns:
         Dictionary: 映射关系字典
     """
-    return make_dict_repeatable(wb, 'Map')
+    return make_dict_repeatable(wb, GL.GL_EXECEL_SHEET_MAP_NAME)
 
 
 if __name__ == "__main__":
